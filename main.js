3beee448e82d14da7e1dda6061f91b2245f79210
mateusdebarba <145573351+mateusdebarba@users.noreply.github.com>
2025-06-17 07:54:16 -0300

    17/06

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i=0; i<botoes.length; i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("10-07-2026t00:00:00")
+const tempoObjetivo1 = new Date("2026-10-06t00:00:00")
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
