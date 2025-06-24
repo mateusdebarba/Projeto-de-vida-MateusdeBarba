3fbcb9c5976f2606c4039410e4cd757135450ee3
mateusdebarba <145573351+mateusdebarba@users.noreply.github.com>
2025-06-24 08:15:36 -0300

    24/06

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ for (let i=0; i<botoes.length; i++){
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-10-06t00:00:00")
 
-contadores{0}.textContent = calculaTempo(tempoObjetivo1);
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
