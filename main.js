80f46df92508094280ec27b7b06bff0919dfe44f
mateusdebarba <145573351+mateusdebarba@users.noreply.github.com>
2025-07-01 07:55:29 -0300

    01/07

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ const tempoObjetivo2 = new Date("2025-11-22t00:00:00")
 const tempoObjetivo3 = new Date("2025-12-18t00:00:00")
 const tempoObjetivo4 = new Date("2026-01-09t00:00:00")
 
-const tempo = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
