0a31609a4157db9499649e4edc82bfe96253d4f1
mateusdebarba <145573351+mateusdebarba@users.noreply.github.com>
2025-07-01 07:54:30 -0300

    01/07

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll("aba-conteudo");
 
 
-for (let i=0; i<botoes.length; i++){
-    botoes[i].onclick = function(){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
 
-        for(let j=0; j< botoes.length; j++){ 
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo"); 
+            textos[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
@@ -20,30 +20,30 @@ const tempoObjetivo2 = new Date("2025-11-22t00:00:00")
 const tempoObjetivo3 = new Date("2025-12-18t00:00:00")
 const tempoObjetivo4 = new Date("2026-01-09t00:00:00")
 
-const tempo = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+const tempo = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-function calculaTempo(tempoObjetivo){
+function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal/1000);
-    let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(minutos/60);
-    let dias = Math.floor(horas/24);
-    
-    segundos %=60;
-    minutos %=60;
-    horas %=60;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 60;
 
     return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
 }
 
-function atualizaCronometro(){
-    for (let i=0; i<contadores.length; i++){
+function atualizaCronometro() {
+    for (let i=0; i<contadores.length; i++) {
         contadores[i].textContent=calculaTempo(tempos[i]);
     }
 }
 
-function comecaCronometro(){
+function comecaCronometro() {
     atualizaCronometro();
     setInterval(atualizaCronometro, 1000);
 }
