04719cd5379da582473ac51b6ee566b5340b3f37
mateusdebarba <145573351+mateusdebarba@users.noreply.github.com>
2025-07-01 08:22:12 -0300

    01/07

--- main.js
+++ main.js
@@ -34,12 +34,16 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 60;
 
-    return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
+    if (tempoFinal > 0) {
+        return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
+    } else {
+        return "Prazo Finalizado!"
+    }
 }
 
 function atualizaCronometro() {
-    for (let i=0; i<contadores.length; i++) {
-        contadores[i].textContent=calculaTempo(tempos[i]);
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i]);
     }
 }
 
